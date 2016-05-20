82b5a04b80df293f42ccc53865cbbbfbd624825c
Bryan Ye <bryanye97@gmail.com>
2016-05-20 16:30:12 +1000

    Add files via upload

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FaceViewController.swift
+//  FaceIt
+//
+//  Created by Bryan Ye on 20/05/2016.
+//  Copyright © 2016 Bryan Ye. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+
+}
+
